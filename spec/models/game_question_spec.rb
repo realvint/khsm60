01c3a306b4708a23dadbb7715acce16b211f2a04
realvint <victor.malukhin@gmail.com>
2022-02-04 23:15:12 +0300

    add test for correct_answer_key

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -1,18 +1,9 @@
-# (c) goodprogrammer.ru
-
 require 'rails_helper'
 
-# Тестовый сценарий для модели игрового вопроса,
-# в идеале весь наш функционал (все методы) должны быть протестированы.
 RSpec.describe GameQuestion, type: :model do
-
-  # задаем локальную переменную game_question, доступную во всех тестах этого сценария
-  # она будет создана на фабрике заново для каждого блока it, где она вызывается
   let(:game_question) { FactoryBot.create(:game_question, a: 2, b: 1, c: 4, d: 3) }
 
-  # группа тестов на игровое состояние объекта вопроса
   context 'game status' do
-    # тест на правильную генерацию хэша с вариантами
     it 'correct .variants' do
       expect(game_question.variants).to eq({'a' => game_question.question.answer2,
                                             'b' => game_question.question.answer1,
@@ -21,18 +12,13 @@ RSpec.describe GameQuestion, type: :model do
     end
 
     it 'correct .answer_correct?' do
-      # именно под буквой b в тесте мы спрятали указатель на верный ответ
       expect(game_question.answer_correct?('b')).to be_truthy
     end
-  end
 
-  # help_hash у нас имеет такой формат:
-  # {
-  #   fifty_fifty: ['a', 'b'], # При использовании подсказски остались варианты a и b
-  #   audience_help: {'a' => 42, 'c' => 37 ...}, # Распределение голосов по вариантам a, b, c, d
-  #   friend_call: 'Василий Петрович считает, что правильный ответ A'
-  # }
-  #
+    it 'correct .correct_answer_key' do
+      expect(game_question.correct_answer_key).to eq 'b'
+    end
+  end
 
   context 'user helpers' do
     it 'correct audience_help' do
