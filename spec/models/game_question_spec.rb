4ffe9aca18abf92db2163b32ff1348ef66d932b3
realvint <victor.malukhin@gmail.com>
2022-02-05 15:12:50 +0300

    refactor game_question_spec

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe GameQuestion, type: :model do
   let(:game_question) { FactoryBot.create(:game_question, a: 2, b: 1, c: 4, d: 3) }
 
-  context 'game status' do
+  describe 'game status' do
     it 'correct .variants' do
       expect(game_question.variants).to eq({'a' => game_question.question.answer2,
                                             'b' => game_question.question.answer1,
@@ -14,30 +14,38 @@ RSpec.describe GameQuestion, type: :model do
     it 'correct .answer_correct?' do
       expect(game_question.answer_correct?('b')).to be_truthy
     end
+  end
 
-    it 'correct .correct_answer_key' do
+  describe '.correct_answer_key' do
+    it 'return correct answer key' do
       expect(game_question.correct_answer_key).to eq 'b'
     end
+  end
 
-    it 'correct .text' do
+  describe '.text' do
+    it ' return correct text' do
       expect(game_question.text).to eq(game_question.question.text)
     end
+  end
 
-    it 'correct .level' do
+  describe '.level' do
+    it 'return correct level' do
       expect(game_question.level).to eq(game_question.question.level)
     end
   end
 
-  context 'user helpers' do
-    it 'correct audience_help' do
-      expect(game_question.help_hash).not_to include(:audience_help)
+  describe 'user helpers' do
+    context 'helpers' do
+      it 'correct audience_help' do
+        expect(game_question.help_hash).not_to include(:audience_help)
 
-      game_question.add_audience_help
+        game_question.add_audience_help
 
-      expect(game_question.help_hash).to include(:audience_help)
+        expect(game_question.help_hash).to include(:audience_help)
 
-      ah = game_question.help_hash[:audience_help]
-      expect(ah.keys).to contain_exactly('a', 'b', 'c', 'd')
+        ah = game_question.help_hash[:audience_help]
+        expect(ah.keys).to contain_exactly('a', 'b', 'c', 'd')
+      end
     end
   end
 end
